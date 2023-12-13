2ac49f2a42909dc991d92a90381d9cedbed15fa1
Vijay-Vaddi <vijayzvaddi@gmail.com>
2023-12-13 21:51:07 +0000

    updates to yf scraping

--- web-scraping/yahoo_finance.py
+++ web-scraping/yahoo_finance.py
@@ -1,17 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://finance.yahoo.com/quote/AMZN"
-headers = {'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
-#without adding user agent yf was giving false/static data.
+def getStockData(orgs):
+    stock_data = []
+    for org in orgs:
+        print(org)
+        url = f"https://finance.yahoo.com/quote/{org}"
+        headers = {'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
+        #without adding user agent yf was giving false/static data.
 
-res = requests.get(url, headers=headers)
-soup = BeautifulSoup(res.text, "html.parser")
+        res = requests.get(url, headers=headers)
+        soup = BeautifulSoup(res.text, "html.parser")
 
-# print(soup.title.text) #to test if its working. a good method and a step. 
-# print(soup)
+        # print(soup.title.text) #to test if its working. a good method and a step. 
+        # print(soup)
+        stock = {
+        'price' : soup.find('fin-streamer', {'class':"Fw(b) Fz(36px) Mb(-4px) D(ib)"}).text,
+        'change' : soup.find('fin-streamer', {'class':"Fw(500) Pstart(8px) Fz(24px)"}).text
+        }
+        stock_data.append(stock)
+        print(stock)
 
-price = soup.find('fin-streamer', {'class':"Fw(b) Fz(36px) Mb(-4px) D(ib)"}).text
-change = soup.find('fin-streamer', {'class':"Fw(500) Pstart(8px) Fz(24px)"}).text
-print(price, change)
 
+company_list = ['AMZN', 'TSLA', 'GME', 'AMD', 'AAPL', 'GOOG']
+
+getStockData(company_list)
