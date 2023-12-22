d2f8ae373c56048b5fec424eb1aad1549f3ef4f0
Vijay-Vaddi <vijayzvaddi@gmail.com>
2023-12-22 20:33:19 +0000

    flask url variable rules

--- web-server/server.py
+++ web-server/server.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template('index.html')
+# @app.route("/")
+# def hello_world():
+#     return render_template('index.html')
+
+#url parameters
+
+@app.route("/<username>/<int:post_id>")
+def url_para(username=None, post_id=None):
+    return render_template('index.html', name=username, id=post_id)
 
 @app.route("/wazzup")
 def wazzup():
