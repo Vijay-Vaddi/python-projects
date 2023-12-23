3e11313d60ab8f6dfbb1f5a2e92aad1d8b8805f6
Vijay-Vaddi <vijayzvaddi@gmail.com>
2023-12-23 11:07:22 +0000

    dynamic url routing

--- web-server/server.py
+++ web-server/server.py
@@ -8,15 +8,32 @@ app = Flask(__name__)
 
 #url parameters
 
-@app.route("/<username>/<int:post_id>")
-def url_para(username=None, post_id=None):
-    return render_template('index.html', name=username, id=post_id)
+# @app.route("/<username>/<int:post_id>")
+# def url_para(username=None, post_id=None):
+#     return render_template('index.html', name=username, id=post_id)
 
-@app.route("/wazzup")
-def wazzup():
-    return "<p>Wazzzaaaaaap My Boyzz!!!!</p>"
+@app.route("/")
+# @app.route("/index.html")
+def home():
+    return render_template('index.html')
 
-@app.route("/about")
-def about():
-    return render_template('about.html')
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
+
+# @app.route("/works.html")
+# def works():
+#     return render_template('works.html')
+
+# @app.route("/work.html")
+# def work():
+#     return render_template('work.html')
+
+# @app.route("/contact.html")
+# def contact():
+#     return render_template('contact.html')
+
+# @app.route("/about.html")
+# def about():
+#     return render_template('about.html')
 
