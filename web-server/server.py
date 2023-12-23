626612e3bac57a4171dbd4135801586ea8179daf
Vijay-Vaddi <vijayzvaddi@gmail.com>
2023-12-23 17:03:51 +0000

    save form to txt file.

--- web-server/server.py
+++ web-server/server.py
@@ -2,12 +2,7 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-# @app.route("/")
-# def hello_world():
-#     return render_template('index.html')
-
 #url parameters
-
 # @app.route("/<username>/<int:post_id>")
 # def url_para(username=None, post_id=None):
 #     return render_template('index.html', name=username, id=post_id)
@@ -21,28 +16,20 @@ def home():
 def html_page(page_name):
     return render_template(page_name)
 
+def save_to_file(data):
+    with open('web-server/database.txt', mode='a') as dataBase:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = dataBase.write(f'{email} {subject} {message}')
+        
+
 @app.route("/submit_form", methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        print(data)
+        save_to_file(data)
         return redirect("/thank_you.html")
     else: 
         return 'something went wong'
-
-# @app.route("/works.html")
-# def works():
-#     return render_template('works.html')
-
-# @app.route("/work.html")
-# def work():
-#     return render_template('work.html')
-
-# @app.route("/contact.html")
-# def contact():
-#     return render_template('contact.html')
-
-# @app.route("/about.html")
-# def about():
-#     return render_template('about.html')
 
