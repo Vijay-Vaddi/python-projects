0e8068c530b373eb1ceb2869befe7586568a341a
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-01-25 19:46:34 +0000

    madlib

--- madlibs.py
+++ madlibs.py
@@ -6,3 +6,12 @@ new_word = 'General Kenobi'
 # print('Hello there {}'.format(new_word))
 # print(f"Hello there {new_word}")
 
+adj = input("Adjective : ")
+verb1 = input("Verb : ")
+verb2 = input("Effect : ")
+res = input("Res : ")
+
+madlibs = f"Computer programming is so {adj}! And it makes me excited all the time \
+I love to {verb1}. It also gives me {verb2} and but I work hard to get {res}"
+
+print(madlibs)
